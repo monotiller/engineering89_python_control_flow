dace1be5b53aebed45dfbad59dec09b25e4a6632
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-06-23 11:28:41 +0100

    End of morning session

--- control_flow.py
+++ control_flow.py
@@ -1,30 +1,21 @@
-# Control flow with if elif and else conditions
-## Loops
-When do we need to use conditions?
+# Syntax
+# if variable condition age:
 
-Syntax:
-```python
-if variable condition age:
-```
-Here's an example:
-```python
 weather = "sunny"
 
 if weather == "sunny": # if this condition is true the next line would execute
     print("Enjoy the weather") # this line needs to be indented inside the code block
-    
+
 if weather == "rainy": # if this condition is true the next line would execute
     print("Take an umbrella") # this line needs to be indented inside the code block
 
 if weather != "rainy": # if this condition is not true the next line would execute
     print("There is no rain, enjoy the sun!") # this line needs to be indented inside the code block
-```
-Let's use our ticket age criteria 
-```python
+
+# Let's use our ticket age criteria
 age = 17
 
 if age < 18: # Checking the condition according to the age given
     print("You are not eligible to watch this film")
 else: # Give the user a different message if they are 18 or over!
     print("Enjoy the film!")
-```
